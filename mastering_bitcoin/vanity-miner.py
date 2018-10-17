6e0dab8ccfce81d62ac9ad16c104c83003e3fd50
heartkilla <artem.official.by@gmail.com>
2018-10-17 21:22:10 +0900

    Add vanity-miner.py

---
+++ mastering_bitcoin/vanity-miner.py
@@ -0,0 +1,14 @@
+import bitcoin
+import os
+import codecs
+
+search = '1kid'
+
+while True:
+	secret = os.urandom(32)
+	address = bitcoin.privkey_to_address(secret)
+	if search in address:
+		break
+
+print('Found vanity address! ', address)
+print('Secret: ', codecs.encode(secret, 'hex').decode())
